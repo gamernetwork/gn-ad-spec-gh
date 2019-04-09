c251a14da51e22cdb580d513ddec8fd016855881
George Francis <george.francis@gamer-network.net>
2019-04-09 10:31:59 +0100

    init

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gamer Network Ad Spec`,
+    description: ``,
+    author: `@gamernetwork`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+  ],
+}
