60cb4c2ee28ac8b894928f5570a646efe0ee8932
George Francis <george.francis@gamer-network.net>
2019-04-09 10:44:05 +0100

    add some markdown content and the ability to read and transform markdown files in Gatsby

---
+++ content/test-section/test.md
@@ -0,0 +1,5 @@
+# Hello! 
+
+This is some test markdown content 
+
+"It has nice punctuation"
--- gatsby-config.js
+++ gatsby-config.js
@@ -5,8 +5,26 @@ module.exports = {
     author: `@gamernetwork`,
   },
   plugins: [
+    // for querying the filesystem
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content`,
+        name: "markdown-pages",
+      },
+    },
+    // transform md
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // use smart punctuation
+        plugins: [`gatsby-remark-smartypants`],
+      },
+    },
+    // for inserting stuff into html head
     `gatsby-plugin-react-helmet`,
+    // image transformations
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
   ],
-}
+};
