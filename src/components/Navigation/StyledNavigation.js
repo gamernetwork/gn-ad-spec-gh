ede086e64b986517daf959012bbea5e3f0415b66
George Francis <george.francis@gamer-network.net>
2019-04-09 13:41:07 +0100

    add css reset and some nav styles

--- src/components/Navigation/StyledNavigation.js
+++ src/components/Navigation/StyledNavigation.js
@@ -2,6 +2,11 @@ import styled from 'styled-components';
 
 const StyledNavigation = styled.nav`
   position: fixed;
+  top: 0;
+  left: 0;
+  width: 18rem;
+  height: 100vh;
+  background: #222;
 `;
 
 export default StyledNavigation;
