d578199b3488bb80ba236c7eb5950a2a5e0d403d
George Francis <george.francis@gamer-network.net>
2019-04-15 11:58:03 +0100

    first pass at mobile nav

--- src/components/Navigation/StyledNavigation.js
+++ src/components/Navigation/StyledNavigation.js
@@ -4,12 +4,11 @@ const StyledNavigation = styled.nav`
   position: fixed;
   top: 0;
   left: 0;
-  width: 100vw;
+  width: var(--nav-width);
   height: 100vh;
-  transform: translateX(-100%);
-  background: #222;
   transition: transform 300ms ease;
   z-index: 1;
+  background: #222;
 
   li {
     list-style: none;
@@ -28,9 +27,45 @@ const StyledNavigation = styled.nav`
     text-decoration: none;
   }
 
-  @media only screen and (min-width: 1024px) {
-    width: var(--nav-width);
-    transform: none;
+  &[aria-hidden='true'] {
+    transform: translateY(calc(100% - 2.5rem));
+  }
+
+  @media only screen and (max-width: 1024px) {
+    width: 100vw;
+    background: #222;
+    height: auto;
+    bottom: 0;
+    top: auto;
+    left: auto;
+    right: 0;
+    transform-origin: 100% 100%;
+    box-shadow: 0px -2px 12px 4px rgba(0, 0, 0, 0.15);
+
+    &:after {
+      position: absolute;
+      top: 0.75rem;
+      left: 50%;
+      transform: translateX(-50%);
+      content: 'MENU';
+      color: #fff;
+      font-weight: 500;
+      letter-spacing: 0.025em;
+      line-height: 1;
+    }
+
+    &[aria-hidden='false'] {
+      &:after {
+        content: 'CLOSE';
+      }
+    }
+
+    ul {
+      text-align: center;
+      margin-left: 0;
+      margin-top: 3rem;
+      margin-bottom: 2rem;
+    }
   }
 `;
 
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,56 +1,87 @@
-import React from 'react';
+import React, { Component } from 'react';
+import WindowSizeListener from 'react-window-size-listener';
 import Scrollspy from 'react-scrollspy';
 import StyledNavigation from './StyledNavigation';
 
-const Navigation = () => (
-  <StyledNavigation>
-    <Scrollspy
-      items={[
-        'introduction',
-        'lead-times',
-        'guidelines',
-        'takeovers',
-        'run-of-network',
-        'html5',
-        'custom-units',
-        'programmatic',
-        'in-stream',
-        'compliance',
-      ]}
-      currentClassName="is-current"
-    >
-      <li>
-        <a href="#introduction">Introduction</a>
-      </li>
-      <li>
-        <a href="#lead-times">Lead Times</a>
-      </li>
-      <li>
-        <a href="#guidelines">Guidelines</a>
-      </li>
-      <li>
-        <a href="#takeovers">Takeovers</a>
-      </li>
-      <li>
-        <a href="#run-of-network">Run Of Network</a>
-      </li>
-      <li>
-        <a href="#html5">HTML5</a>
-      </li>
-      <li>
-        <a href="#custom-units">Custom Units</a>
-      </li>
-      <li>
-        <a href="#programmatic">Programmatic</a>
-      </li>
-      <li>
-        <a href="#in-stream">In-Stream</a>
-      </li>
-      <li>
-        <a href="#compliance">Compliance</a>
-      </li>
-    </Scrollspy>
-  </StyledNavigation>
-);
+class Navigation extends Component {
+  state = {
+    windowWidth: window.innerWidth,
+    hideNav: false,
+  };
+
+  onResize = windowSize => {
+    console.log(windowSize);
+    this.setState(
+      {
+        windowWidth: windowSize,
+      },
+      () => {
+        const { windowWidth } = this.state;
+        let hideNav;
+        windowWidth <= 1024 ? (hideNav = true) : (hideNav = false);
+        this.setState({
+          hideNav,
+        });
+      }
+    );
+  };
+
+  render() {
+    const { hideNav } = this.state;
+    return (
+      <StyledNavigation aria-hidden={hideNav}>
+        <WindowSizeListener
+          onResize={windowSize => this.onResize(windowSize.windowWidth)}
+        />
+        <Scrollspy
+          items={[
+            'introduction',
+            'lead-times',
+            'guidelines',
+            'takeovers',
+            'run-of-network',
+            'html5',
+            'custom-units',
+            'programmatic',
+            'in-stream',
+            'compliance',
+          ]}
+          currentClassName="is-current"
+        >
+          <li>
+            <a href="#introduction">Introduction</a>
+          </li>
+          <li>
+            <a href="#lead-times">Lead Times</a>
+          </li>
+          <li>
+            <a href="#guidelines">Guidelines</a>
+          </li>
+          <li>
+            <a href="#takeovers">Takeovers</a>
+          </li>
+          <li>
+            <a href="#run-of-network">Run Of Network</a>
+          </li>
+          <li>
+            <a href="#html5">HTML5</a>
+          </li>
+          <li>
+            <a href="#custom-units">Custom Units</a>
+          </li>
+          <li>
+            <a href="#programmatic">Programmatic</a>
+          </li>
+          <li>
+            <a href="#in-stream">In-Stream</a>
+          </li>
+          <li>
+            <a href="#compliance">Compliance</a>
+          </li>
+        </Scrollspy>
+      </StyledNavigation>
+    );
+  }
+}
 
 export default Navigation;
